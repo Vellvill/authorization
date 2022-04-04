ce635d3fbf2cdd609c40bae7960c7785463bb0b7
Gerasimov Timofey <gerasimoff.timofeii@yandex.ru>
2022-04-05 00:49:30 +0300

    db

--- internal/server/config.go
+++ internal/server/config.go
@@ -1,17 +1,17 @@
 package server
 
+import "auth/internal/store"
+
 type Config struct {
-	Port     string `yaml:"port"`
-	LogLevel string `yaml:"log_level"`
-	Store    struct {
-		DSN string `yaml:"dsn"`
-	} `yaml:"store"`
+	Port     string `default:":8080" yaml:"port"`
+	LogLevel string `default:"debug" yaml:"log_level"`
+	Store    *store.Config
 }
 
 func GetConfig() *Config {
 	return &Config{
 		Port:     ":8080",
 		LogLevel: "debug",
+		Store:    store.NewConfig(),
 	}
-
 }
--- internal/store/store.go
+++ internal/store/store.go
@@ -1,24 +1,13 @@
 package store
 
 import (
-	"context"
 	"database/sql"
-	"github.com/jackc/pgconn"
-	"github.com/jackc/pgx/v4"
-
 	_ "github.com/lib/pq"
 )
 
 type Store struct {
 	config *Config
 	db     *sql.DB
-}
-
-type Client interface {
-	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
-	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
-	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
-	Begin(ctx context.Context) (pgx.Tx, error)
 }
 
 func New(config *Config) *Store {
